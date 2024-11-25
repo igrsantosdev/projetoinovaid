eda14658235e0503ccf6a86bf622a43267175161
igrteste <igrteste@gmail.com>
2024-11-25 10:42:53 -0300

    testando git outra maquina2

---
+++ inovaid2/src/model/Produto.java
@@ -0,0 +1,53 @@
+
+package model;
+
+
+public class Produto {
+    
+    private int id;
+    private String nome;
+    private int codigo;
+    private int quantidade;
+    private double preco;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+}
